0cfa19bcba667567207085fc39e788f985a92828
Sören Havemester <reshave@me.com>
2024-04-25 10:17:30 +0200

    Initial Commit

---
+++ Hallo Ume/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hallo Ume
+//
+//  Created by Sören Havemester on 25.04.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hallo Ume/Hallo_UmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hallo_UmeApp.swift
+//  Hallo Ume
+//
+//  Created by Sören Havemester on 25.04.24.
+//
+
+import SwiftUI
+
+@main
+struct Hallo_UmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
